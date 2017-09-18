9cd9484018b9adef41f298d8208369826bfddec1
yghtest <948170910@qq.com>
2017-09-18 08:39:04 +0800

    commit

---
+++ redis_day01/src/main/java/redis_demo1/Demo1.java
@@ -0,0 +1,30 @@
+package redis_demo1;
+
+import java.util.List;
+import java.util.Set;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * 
+ * @author yuangg
+ *
+ * @date 2017年9月18日
+ */
+public class Demo1 {
+	public static JedisPool pool;
+	public static void main(String[] args) {
+		pool = new JedisPool(new JedisPoolConfig(),"localhost");
+		Jedis jedis = pool.getResource();
+		jedis.set("foo", "bar");
+		String foobar = jedis.get("foo");
+		System.out.println(foobar);
+		jedis.zadd("sose",10,"car");
+//		jedis.zadd"sose", 0,"car");
+		Set<String> sose = jedis.zrange("sose", 0, -1);
+		System.out.println(sose);
+		pool.close();
+	}
+}
